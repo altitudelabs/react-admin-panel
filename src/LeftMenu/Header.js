a98000685ba5fcbd04827b983b724a89d1f10149
Varun Vasudevan <the.flazer@gmail.com>
2017-05-02 18:42:27 +0800

    Fixed conflicts

---
+++ src/LeftMenu/Header.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
+const Header = (props) => {
+  const itemClass = classNames(
+    'header',
+  );
+  return (
+    <div
+      className={itemClass}
+    >
+      <div
+        className={props.headerLogoClassName}
+      />
+      <img src={props.headerLogoSrc} />
+    </div>
+  );
+};
+
+Header.defaultProps = {
+};
+
+Header.propTypes = {
+  headerLogoClassName: PropTypes.string,
+};
+
+export default Header;
